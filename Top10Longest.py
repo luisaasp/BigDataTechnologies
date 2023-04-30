a994a96b8d7d766672b12e663c720f810822df17
luisaasp <132214895+luisaasp@users.noreply.github.com>
2023-04-30 19:08:28 +0100

    Update Top10Longest.py

--- Top10Longest.py
+++ Top10Longest.py
@@ -5,11 +5,11 @@ import csv
 
 class BestMovies(MRJob):
 
-MIN_COUNT = 10
-SHOW_LIMIT = 10
+    MIN_COUNT = 10
+    SHOW_LIMIT = 10
 
 
-def movie_title(self, movie_id):
+    def movie_title(self, movie_id):
         '''
         Convert from movie id to movie title
         '''
@@ -21,36 +21,36 @@ def movie_title(self, movie_id):
                     return line[1]
 
 
-def steps(self):
-    '''
-    Pipeline of MapReduce tasks
-    '''
-    return [
-        MRStep(mapper=self.mapper1, reducer=self.reducer1),
-        MRStep(mapper=self.mapper2, reducer=self.reducer2)]
+    def steps(self):
+        '''
+        Pipeline of MapReduce tasks
+        '''
+        return [
+            MRStep(mapper=self.mapper1, reducer=self.reducer1),
+            MRStep(mapper=self.mapper2, reducer=self.reducer2)]
 
-def mapper1(self, _, line):
-    (user_id, movie_id, rating, timestamp) = line.split('\t')
-    yield movie_id, rating
+    def mapper1(self, _, line):
+        (user_id, movie_id, rating, timestamp) = line.split('\t')
+        yield movie_id, rating
 
-def reducer1(self, movie_id, ratings):
-    sum_ratings, count = 0, 0
-    for r in ratings:
-        sum_ratings += int(r)
-        count += 1
-        if count >= self.MIN_COUNT:
-            yield movie_id, sum_ratings/float(count)
+    def reducer1(self, movie_id, ratings):
+        sum_ratings, count = 0, 0
+        for r in ratings:
+            sum_ratings += int(r)
+            count += 1
+            if count >= self.MIN_COUNT:
+                yield movie_id, sum_ratings/float(count)
 
 
-def mapper2(self, movie_id, avg_rating):
-    yield None, (avg_rating, movie_id)
+    def mapper2(self, movie_id, avg_rating):
+        yield None, (avg_rating, movie_id)
 
-def reducer2(self, _, values):
-    i = 0
-    for avg_rating, movie_id in sorted(values, reverse=True):
-        i += 1
-        if i <= self.SHOW_LIMIT:
-            yield movie_id, (self.movie_title(int(movie_id)), avg_rating)
+    def reducer2(self, _, values):
+        i = 0
+        for avg_rating, movie_id in sorted(values, reverse=True):
+            i += 1
+            if i <= self.SHOW_LIMIT:
+                yield movie_id, (self.movie_title(int(movie_id)), avg_rating)
 
 
 if __name__ == '__main__':
